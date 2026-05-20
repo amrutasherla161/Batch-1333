cb06d01efcd1ecbd9759a9932afc4f7034b96d9e
Amruta Sherla <sherlaamruta161@gmail.com>
2026-05-20 16:12:19 +0530

    Add files via upload

---
+++ slice.py
@@ -0,0 +1,19 @@
+name = "Amruta Sham Sherla"
+
+# 1. Start +ve, End +ve, Step +ve
+print(name[0:6:1])     
+
+# 2. Start +ve, End +ve, Step -ve
+print(name[6:0:-1])    
+
+# 3. Start -ve, End +ve, Step +ve
+print(name[-18:6:1])   
+
+# 4. Start +ve, End -ve, Step +ve
+print(name[0:-7:1])    
+
+# 5. Start -ve, End -ve, Step +ve
+print(name[-18:-1:1])  
+
+# 6. Start -ve, End -ve, Step -ve
+print(name[-1:-19:-1])

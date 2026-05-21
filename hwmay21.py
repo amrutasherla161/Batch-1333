624a35ba925bbaeca6fd8cc195bc8a8a61d5048e
Amruta Sherla <sherlaamruta161@gmail.com>
2026-05-21 22:55:07 +0530

    Add files via upload

---
+++ hwmay21.py
@@ -0,0 +1,11 @@
+student_name = ["Sai","Rahul","Rohit","Sita","Gita","Ravi","Kumar"]
+
+print(student_name[2])
+#task2
+student_name = ["Sai","Rahul","Rohit","Sita","Gita","Ravi","Kumar"]
+
+print(student_name[2][::-1])
+#task3
+student_name = ["Sai","Rahul","Rohit","Sita","Gita","Ravi","Kumar"]
+
+print(student_name[2][2:3])

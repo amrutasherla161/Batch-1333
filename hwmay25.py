7b276ced95ed9cfbab715a77e251072a359f1258
Amruta Sherla <sherlaamruta161@gmail.com>
2026-05-26 10:13:53 +0530

    Add files via upload

---
+++ hwmay25.py
@@ -0,0 +1,6 @@
+
+salary = [67000, 45000, 78000, 55000, 28000]
+
+print("Minimum Salary =", min(salary))
+print("Maximum Salary =", max(salary))
+print("Average Salary =", sum(salary) / len(salary))

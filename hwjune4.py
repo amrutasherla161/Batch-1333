2186f8ad630178fc0de2f99a90466e29d3952755
Amruta Sherla <sherlaamruta161@gmail.com>
2026-06-05 10:00:24 +0530

    Add files via upload

---
+++ hwjune4.py
@@ -0,0 +1,11 @@
+movie_db ={
+    "ArjunReddy":"Vijay Deverakonda",
+    "Jersey":"Nani",
+    "Mission Mangal":"Vidya Balan",
+    "12th Fail":"Vikrant Massey ",
+    "The Pursuit of Happyness":"Will Smith"
+}
+for movie, actor in movie_db.items():
+      print("Movie Name:", movie)
+      print("Actor Name:", actor)
+      print("    ")

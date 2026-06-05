f441365c7f12cb25557285b7fa2f67ca012ab083
Amruta Sherla <sherlaamruta161@gmail.com>
2026-06-05 15:58:01 +0530

    Refactor movie_db to include hero and cast details

--- hwjune4.py
+++ hwjune4.py
@@ -1,11 +1,28 @@
-movie_db ={
-    "ArjunReddy":"Vijay Deverakonda",
-    "Jersey":"Nani",
-    "Mission Mangal":"Vidya Balan",
-    "12th Fail":"Vikrant Massey ",
-    "The Pursuit of Happyness":"Will Smith"
+movie_db = {
+    "Arjun Reddy": {
+        "Hero": "Vijay Deverakonda",
+        "Cast": "Shalini Pandey"
+    },
+    "Jersey": {
+        "Hero": "Nani",
+        "Cast": "Shraddha Srinath"
+    },
+    "Mission Mangal": {
+        "Hero": "Akshay Kumar",
+        "Cast": "Vidya Balan"
+    },
+    "12th Fail": {
+        "Hero": "Vikrant Massey",
+        "Cast": "Medha Shankar"
+    },
+    "The Pursuit of Happyness": {
+        "Hero": "Will Smith",
+        "Cast": "Jaden Smith"
+    }
 }
-for movie, actor in movie_db.items():
-      print("Movie Name:", movie)
-      print("Actor Name:", actor)
-      print("    ")
+
+for movie, details in movie_db.items():
+    print("Movie Name :", movie)
+    print("Hero       :", details["Hero"])
+    print("Cast       :", details["Cast"])
+    print()
